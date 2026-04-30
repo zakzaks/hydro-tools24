6f31b2c33178d09675bb81bcbd2b806a2ce5b19f
zakzaks <ariefn82@gmail.com>
2026-04-30 13:13:30 +0700

    feat: integrate Tailwind CSS and refactor App component
    - Added Tailwind CSS as a dependency and configured it in vite.config.js.
    - Removed existing CSS styles from App.css and index.css, replacing them with Tailwind utility classes.
    - Created a new DepthMeasurement component for area input fields.
    - Removed unused assets (react.svg, vite.svg, hero.png) and corresponding styles.
    - Simplified App component to render only the DepthMeasurement component.

---
+++ src/components/DepthMeasurement.jsx
@@ -0,0 +1,18 @@
+export default function DepthMeasurement() {
+	return (
+		<section>
+			<div className="flex flex-col gap-4 ">
+				<label htmlFor="areaWidth">Area Width</label>
+				<input className="border border-black" type="text" />
+			</div>
+			<div className="flex flex-col gap-4 ">
+				<label htmlFor="areaWidth">Area Width</label>
+				<input className="border border-black" type="text" />
+			</div>
+			<div className="flex flex-col gap-4 ">
+				<label htmlFor="areaWidth">Area Width</label>
+				<input className="border border-black" type="text" />
+			</div>
+		</section>
+	);
+}
