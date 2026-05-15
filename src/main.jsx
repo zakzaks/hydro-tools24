90f4b61b649dad7ae09a0cb338a350678233780c
zakzaks <ariefn82@gmail.com>
2026-05-15 15:46:37 +0700

    feat: add react-router for routing and update main rendering logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
 import "./index.css";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
+const router = createBrowserRouter([{ path: "/", element: <App /> }]);
+const root = document.getElementById("root");
+
+createRoot(root).render(
 	<StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</StrictMode>,
 );
