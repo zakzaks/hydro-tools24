776a56c59397f5e84e6d44f6a901c9991f3ba01d
zakzaks <ariefn82@gmail.com>
2026-05-16 11:39:51 +0700

    feat: replace Link with NavLink for active link styling in Layout component

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,12 +1,22 @@
-import { Link, Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
 
 import Footer from "./components/Footer";
 
 function Layout() {
 	return (
 		<div className="flex min-h-screen flex-col bg-linear-to-br from-slate-100 to-blue-50">
-			<Link to="/sbes">SBES Measurement</Link>
-			<Link to="/mbes">MBES Measurement</Link>
+			<NavLink
+				to="/sbes"
+				className={({ isActive }) => (isActive ? "underline" : undefined)}
+			>
+				SBES Measurement
+			</NavLink>
+			<NavLink
+				to="/mbes"
+				className={({ isActive }) => (isActive ? "underline" : undefined)}
+			>
+				MBES Measurement
+			</NavLink>
 			<main className="flex-1">
 				<Outlet />
 			</main>
